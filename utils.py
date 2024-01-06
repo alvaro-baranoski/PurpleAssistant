e193c2e8b71196052b415d2f9b7ba1ccfa6d0e1a
Álvaro José Baranoski <alvarojosebaranoski01@gmail.com>
2024-01-05 21:10:28 -0300

    delete everything

--- utils.py
+++
@@ -1,9 +0,0 @@
-from pathlib import Path
-
-class Utils(object):
-    def __init__(self) -> None:
-        pass
-        
-    @staticmethod
-    def get_root_directory():
-        return Path(__file__).parent
